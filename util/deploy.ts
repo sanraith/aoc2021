74319d4afd1026fd6a677ac9468d9ebaccae7804
Soma Zsják <sanraith@users.noreply.github.com>
2021-10-25 22:00:37 +0200

    Added deploy script for github pages.

---
+++ util/deploy.ts
@@ -0,0 +1,21 @@
+import * as fsx from 'fs-extra';
+import { runChildProcessAsync } from './helpers';
+
+/**
+ * Rebuilds the react app, and copies the output to docs/.
+ * Used for github-pages deployment.
+ */
+async function deploy() {
+    console.log('Deleting docs/...');
+    await fsx.rmdir('docs/', { recursive: true });
+
+    console.log('Building...');
+    await runChildProcessAsync('npm run build');
+
+    console.log('Copying build/ to docs/...');
+    await fsx.copy('build/', 'docs/', { recursive: true });
+
+    console.log('Done.');
+}
+
+deploy();
---
+++ util/helpers.ts
@@ -0,0 +1,10 @@
+import * as childProcess from 'child_process';
+
+export async function runChildProcessAsync(command: string, pipeOutput?: boolean): Promise<void> {
+    pipeOutput = pipeOutput ?? true;
+    return new Promise<void>(resolve => {
+        const child = childProcess.exec(command);
+        if (pipeOutput) { child.stdout.pipe(process.stdout); }
+        child.on('exit', () => resolve());
+    });
+}
