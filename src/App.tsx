8a45824bae1421ea995e5fb74dff64446a8bed21
Soma Zsják <sanraith@users.noreply.github.com>
2021-10-26 23:40:57 +0200

    React popup working.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function calendarCellId(dayNumber: number) { return `cd${dayNumber}`; }
@@ -8,26 +8,67 @@ interface PopupProps {
     onClosed: () => void;
 }
 
-function Popup({ cellId, onClosed }: PopupProps) {
-    let style: React.CSSProperties = {
-        visibility: cellId ? 'visible' : 'hidden'
-    };
+function getPopupStyle(cellId: string | null, prevCellId: string | null, state: 'start' | 'end'): React.CSSProperties {
+    // Skip invalid case.
+    if (!cellId && !prevCellId) { return { visibility: 'hidden' }; }
 
-    if (cellId) {
-        const cell = document.getElementById(cellId)!;
-        const cellRect = cell.getBoundingClientRect();
-        style = {
-            ...style,
+    // If we switch between cells, pretend that we closed the previous one.
+    if (cellId && prevCellId) { prevCellId = null; }
+
+    const cell = document.getElementById(cellId ? cellId : prevCellId!)!;
+    const cellRect = cell.getBoundingClientRect();
+    const isOpenDirection = state === 'start' && cellId || state === 'end' && prevCellId;
+    if (isOpenDirection) {
+        return {
+            visibility: 'hidden',
             left: cellRect.x,
             top: cellRect.y,
             width: cellRect.width + 'px',
             height: cellRect.height + 'px'
         };
-        console.log(style);
+    } else {
+        return {
+            visibility: 'visible',
+            left: Math.max(11, cellRect.x - 50),
+            top: Math.max(11, cellRect.y - 50),
+            width: cellRect.width + 100,
+            height: cellRect.height + 100
+        };
     }
+}
 
+function Popup({ cellId, onClosed }: PopupProps) {
+    console.log(cellId + ' render in');
+    const prevCellIdRef = useRef<string | null>(null);
+    const [style, setStyle] = useState<React.CSSProperties>();
+    const [hasTransition, setHasTransition] = useState(false);
+    const [animationStep, setAnimationStep] = useState(0);
+
+    // If cellId changes, turn off transition and position popup.
+    useEffect(() => {
+        console.log(cellId + ' anim-0 in');
+        const localStyle = getPopupStyle(cellId, prevCellIdRef.current, 'start');
+        setHasTransition(false);
+        setStyle(localStyle);
+        setAnimationStep(1);
+        console.log(cellId + ' anim-0 out');
+    }, [cellId]);
+
+    // After initial render, turn on transition and zoom in.
+    useEffect(() => {
+        if (animationStep !== 1) { return; }
+        console.log(cellId + ' anim-1 in');
+        setHasTransition(true);
+        setStyle(getPopupStyle(cellId, prevCellIdRef.current, 'end'));
+        setAnimationStep(0);
+        console.log(cellId + ' anim-1 out');
+        prevCellIdRef.current = cellId;
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [animationStep]);
+
+    console.log(cellId + ' render out');
     return (
-        <div id="popup" className="popup" style={style} onClick={() => onClosed()}>
+        <div id="popup" className={'popup' + (hasTransition ? '' : ' notransition')} style={style} onClick={() => onClosed()}>
             alma
         </div>
     );
