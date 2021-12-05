51273ec96c769402dd94be8a84e108db24e13fd6
Soma Zsják <sanraith@users.noreply.github.com>
2021-12-05 16:53:55 +0100

    Optimized day 5 solution.

--- src/solutions/day05.ts
+++ src/solutions/day05.ts
@@ -2,7 +2,7 @@ import { regexMatches } from '../core/helpers';
 import SolutionBase from '../core/solutionBase';
 import { solutionInfo } from '../core/solutionInfo';
 
-interface Pipe { x1: number, y1: number, x2: number, y2: number; }
+interface Pipe { x1: number; y1: number; x2: number; y2: number; }
 
 @solutionInfo({
     day: 5,
@@ -13,8 +13,7 @@ export class Day05 extends SolutionBase {
     protected part1(): string | number {
         const pipes = this.parsePipes();
         const map = this.createMap(pipes, 'skip diagonals');
-        const overlapCount = map.reduce((a, x) => a +
-            x.reduce((a, x) => a + (x > 1 ? 1 : 0), 0), 0);
+        const overlapCount = this.countOverlaps(map);
 
         return overlapCount;
     }
@@ -22,50 +21,40 @@ export class Day05 extends SolutionBase {
     protected part2(): string | number {
         const pipes = this.parsePipes();
         const map = this.createMap(pipes, 'include diagonals');
-        const overlapCount = map.reduce((a, row) => a +
-            row.reduce((a, x) => a + (x > 1 ? 1 : 0), 0), 0);
+        const overlapCount = this.countOverlaps(map);
 
         return overlapCount;
+    }
+
+    private countOverlaps(map: number[][]): number {
+        return map.reduce((a, row) => a + row.reduce((a, x) => a + (x > 1 ? 1 : 0), 0), 0);
     }
 
     private createMap(pipes: Pipe[], mode: 'skip diagonals' | 'include diagonals'): number[][] {
         const map: number[][] = [];
         const skipDiagonals = mode === 'skip diagonals';
         for (const { x1, y1, x2, y2 } of pipes) {
-            const isDiagonal = x1 !== x2 && y1 !== y2;
-            if (isDiagonal) {
-                if (skipDiagonals) { continue; }
-                const yCoords = [...this.iterate(y1, y2)];
-                const xCoords = [...this.iterate(x1, x2)];
-                const coords = yCoords.map((y, i) => ({ x: xCoords[i], y }));
-                for (const { x, y } of coords) {
-                    if (map[y] === undefined) { map[y] = []; }
-                    if (map[y][x] === undefined) { map[y][x] = 0; }
-                    map[y][x]++;
-                }
-            } else {
-                for (const y of this.iterate(y1, y2)) {
-                    for (const x of this.iterate(x1, x2)) {
-                        if (map[y] === undefined) { map[y] = []; }
-                        if (map[y][x] === undefined) { map[y][x] = 0; }
-                        map[y][x]++;
-                    }
-                }
+            if (skipDiagonals && x1 !== x2 && y1 !== y2) {
+                continue;
             }
+            this.drawLine(map, x1, y1, x2, y2);
         }
 
         return map;
     }
 
-    private *iterate(from: number, to: number) {
-        if (to >= from) {
-            for (let i = from; i <= to; i++) {
-                yield i;
-            }
-        } else {
-            for (let i = from; i >= to; i--) {
-                yield i;
-            }
+    private drawLine(map: number[][], x1: number, y1: number, x2: number, y2: number) {
+        const dx = Math.sign(x2 - x1);
+        const dy = Math.sign(y2 - y1);
+
+        let [x, y] = [null, null] as (number | null)[];
+        while (x !== x2 || y !== y2) {
+            x = (x ?? x1 - dx) + dx;
+            y = (y ?? y1 - dy) + dy;
+
+            if (map[y] === undefined) { map[y] = []; }
+            if (map[y][x] === undefined) { map[y][x] = 0; }
+            map[y][x]++;
         }
     }
 
