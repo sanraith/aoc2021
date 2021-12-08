5a96287294179918251b297b89e4d09716e83509
Soma Zsják <sanraith@users.noreply.github.com>
2021-12-08 15:17:57 +0100

    Refactored day 8 solution.

--- src/solutions/day08.ts
+++ src/solutions/day08.ts
@@ -2,15 +2,13 @@ import { intersectSets, regexMatches } from '../core/helpers';
 import SolutionBase from '../core/solutionBase';
 import { solutionInfo } from '../core/solutionInfo';
 
-interface Record { patterns: string[], digits: string[]; }
+type Display = { patterns: string[]; digits: string[]; };
+type VagueSegmentMap = { from: string; to: string[]; }[];
+type SegmentMap = { [from: string]: string; };
+type ReadOnlySet<T> = { has: (item: T) => boolean; };
 
-const originalDigits = ['abcefg', 'cf', 'acdeg', 'acdfg', 'bcdf', 'abdfg', 'abdefg', 'acf', 'abcdefg', 'abcdfg'];
-const originalDigitSet = new Set(originalDigits);
-const originalDigitsByLength = new Map<number, { number: number; digits: string[]; }[]>();
-originalDigits.forEach((x, i) => {
-    if (!originalDigitsByLength.has(x.length)) { originalDigitsByLength.set(x.length, []); }
-    originalDigitsByLength.get(x.length)!.push({ number: i, digits: x.split('') });
-});
+const digitSegments: readonly string[] = ['abcefg', 'cf', 'acdeg', 'acdfg', 'bcdf', 'abdfg', 'abdefg', 'acf', 'abcdefg', 'abcdfg'];
+const digitSegmentSet: ReadOnlySet<string> = new Set(digitSegments);
 
 @solutionInfo({
     day: 8,
@@ -20,87 +18,91 @@ export class Day08 extends SolutionBase {
 
     protected part1(): number {
         const uniqueLengths = new Set([2, 3, 4, 7]);
-        const records = this.parseInput();
-        const uniqueCount = records.reduce((a, r) => a +
+        const displays = this.parseInput();
+        const uniqueCount = displays.reduce((a, r) => a +
             r.digits.reduce((a, d) => a + (uniqueLengths.has(d.length) ? 1 : 0), 0), 0);
 
         return uniqueCount;
     }
 
     protected part2(): number {
-        const records = this.parseInput();
-        const sum = records.reduce((a, record) => a + this.decodeDisplay(record), 0);
+        const displays = this.parseInput();
+        const sum = displays.reduce((a, display, i) => {
+            this.updateProgress(i / displays.length);
+            return a + this.decodeDisplayValue(display);
+        }, 0);
 
         return sum;
     }
 
-    private decodeDisplay({ patterns, digits }: Record) {
-        const maps = this.createCharacterMaps(patterns);
-        const sortedMaps = [...maps.entries()].map(x => [x[0], [...x[1]]] as [string, string[]]).sort((a, b) => a[1].length - b[1].length);
-        const map = this.eliminate(sortedMaps, patterns);
-        if (!map) { this.noSolution(); }
+    /** Decodes the value shown on the display. */
+    private decodeDisplayValue({ patterns, digits }: Display): number {
+        const vagueSegmentMap = this.generateVagueSegmentMap(patterns);
+        const segmentMap = this.resolveSegmentMap(vagueSegmentMap, patterns);
+        if (!segmentMap) { this.noSolution(); }
 
-        const number = parseInt(digits.map(x => this.decodeDigit(x, map)).join(''));
-
-        return number;
+        const displayValue = parseInt(digits.map(x => this.decodeDigit(x, segmentMap)).join(''));
+        return displayValue;
     }
 
-    private eliminate(charMap: [string, string[] | string][], patterns: string[], index = 0): { [key: string]: string; } | null {
-        if (index === charMap.length) {
-            const completedMap = Object.fromEntries(charMap) as { [key: string]: string; };
-            if (this.validate(patterns, completedMap)) {
-                return completedMap;
-            } else {
-                return null;
-            }
+    /** Finds the correct mapping from the initial segment map using backtrack. */
+    private resolveSegmentMap(vagueMap: VagueSegmentMap, patterns: string[], index = 0): SegmentMap | null {
+        if (index === vagueMap.length) {
+            const completedMap = Object.fromEntries(vagueMap.map(x => [x.from, x.to[0]]));
+            return this.isSegmentMapValid(completedMap, patterns) ? completedMap : null;
         }
 
-        const chars = [...charMap[index][1]];
-        for (const char of chars) {
-            const nextCharMap = [
-                ...charMap.slice(0, index),
-                [charMap[index][0], char],
-                ...charMap.slice(index + 1).map(x => [x[0], (x[1] as string[]).filter(c => c !== char)])
-            ] as [string, string[] | string][];
+        const candidateChars = vagueMap[index].to;
+        for (const selectedChar of candidateChars) {
+            const nextMap = [
+                ...vagueMap.slice(0, index),
+                { from: vagueMap[index].from, to: [selectedChar] },
+                ...vagueMap.slice(index + 1).map(x => ({
+                    from: x.from,
+                    to: x.to.filter(c => c !== selectedChar)
+                }))
+            ];
 
-            const result = this.eliminate(nextCharMap, patterns, index + 1);
-            if (result) {
-                return result;
-            }
+            const result = this.resolveSegmentMap(nextMap, patterns, index + 1);
+            if (result) { return result; }
         }
 
         return null;
     }
 
-    private validate(patterns: string[], charMap: { [from: string]: string; }): boolean {
-        for (const pattern of patterns) {
-            const mapped = pattern.split('').map(x => charMap[x]).sort().join('');
-            if (!originalDigitSet.has(mapped)) {
-                return false;
-            }
-        }
-        return true;
+    /** Checks if each pattern can be mapped to a digit using the segment map. */
+    private isSegmentMapValid(map: SegmentMap, patterns: string[]): boolean {
+        return patterns.every(p => digitSegmentSet.has(this.decodeSegments(p, map)));
     }
 
-    private decodeDigit(digitStr: string, charMap: { [from: string]: string; }): number {
-        const mapped = digitStr.split('').map(x => charMap[x]).sort().join('');
-        const digit = originalDigits.indexOf(mapped);
-
-        return digit;
+    /** Decodes a segment string and finds to the corresponding digit. */
+    private decodeDigit(segments: string, map: SegmentMap): number {
+        return digitSegments.indexOf(this.decodeSegments(segments, map));
     }
 
-    private createCharacterMaps(patterns: string[]) {
+    /** Decodes an encoded segment string. */
+    private decodeSegments(segments: string, map: SegmentMap): string {
+        return segments.split('').map(x => map[x]).sort().join('');
+    }
+
+    /** Generates a vague segment map where each character can have multiple possible mappings. */
+    private generateVagueSegmentMap(patterns: string[]): VagueSegmentMap {
+        const digitsSegmentsByLength = new Map<number, string[][]>(Array(8).fill(0).map((_, i) => [i, []]));
+        digitSegments.forEach(x => digitsSegmentsByLength.get(x.length)!.push(x.split('')));
+
         const resultMap = new Map<string, Set<string>>();
         for (const pattern of patterns) {
+            // Generate possible segment mappings based on pattern
             const patternMap = new Map<string, Set<string>>();
-            const validDigits = originalDigitsByLength.get(pattern.length)!;
+            const validDigits = digitsSegmentsByLength.get(pattern.length)!;
             pattern.split('').forEach(patternChar =>
-                validDigits.forEach(vd => vd.digits.forEach(digitChar => {
+                validDigits.forEach(vd => vd.forEach(digitChar => {
                     if (!patternMap.has(patternChar)) { patternMap.set(patternChar, new Set()); }
                     patternMap.get(patternChar)!.add(digitChar);
                 }))
             );
 
+            // Intersect with previous mappings
             for (const [key, newChars] of patternMap.entries()) {
                 if (!resultMap.has(key)) {
                     resultMap.set(key, newChars);
@@ -111,17 +113,20 @@ export class Day08 extends SolutionBase {
             }
         }
 
-        return resultMap;
+        // Generate sorted vague segment map
+        return [...resultMap.entries()]
+            .map(x => ({ from: x[0], to: [...x[1]] }))
+            .sort((a, b) => a.to.length - b.to.length);
     }
 
-    private parseInput(): Record[] {
-        const records = regexMatches(/(.+) \| (.+)/g, this.input).map(lineMatch => {
-            const [, patternsStr, digitsStr] = lineMatch;
+    private parseInput(): Display[] {
+        const displays = regexMatches(/(.+) \| (.+)/g, this.input).map(displayMatch => {
+            const [, patternsStr, digitsStr] = displayMatch;
             const patterns = regexMatches(/[a-g]+/g, patternsStr).map(x => x[0]);
             const digits = regexMatches(/[a-g]+/g, digitsStr).map(x => x[0]);
             return { patterns, digits };
         });
 
-        return records;
+        return displays;
     }
 }
