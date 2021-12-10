e985b417ad747b536422087aea1dd68c7ebae913
Soma Zsják <sanraith@users.noreply.github.com>
2021-12-10 22:05:59 +0100

    Added day 10 doodle. Updated deploy steps order.

--- util/deploy.ts
+++ util/deploy.ts
@@ -6,11 +6,11 @@ import { runChildProcessAsync } from './helpers';
  * Used for github-pages deployment.
  */
 async function deploy() {
-    console.log('Deleting docs/...');
-    await fsx.rmdir('docs/', { recursive: true });
-
     console.log('Building...');
     await runChildProcessAsync('npm run build');
+
+    console.log('Deleting docs/...');
+    await fsx.rmdir('docs/', { recursive: true });
 
     console.log('Copying build/ to docs/...');
     await fsx.copy('build/', 'docs/', { recursive: true });
