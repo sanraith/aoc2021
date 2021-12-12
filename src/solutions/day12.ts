4d277f48a80ffe2264a58c51c76a8298346f92a7
Soma Zsják <sanraith@users.noreply.github.com>
2021-12-12 11:16:16 +0100

    WIP day 12 solution.

--- src/solutions/day12.ts
+++ src/solutions/day12.ts
@@ -1,3 +1,4 @@
+import { regexMatches } from '../core/helpers';
 import SolutionBase from '../core/solutionBase';
 import { solutionInfo } from '../core/solutionInfo';
 
@@ -7,11 +8,47 @@ import { solutionInfo } from '../core/solutionInfo';
 })
 export class Day12 extends SolutionBase {
 
-    protected part1(): string | number {
+    protected part1(): number {
+        const { numberMap, start, end } = this.parseInput();
         this.noSolution();
     }
 
-    protected part2(): string | number {
+    protected part2(): number {
         this.noSolution();
+    }
+
+    private parseInput() {
+        const caves = regexMatches(/(\w+)-(\w+)/g, this.input).map(lineMatch => {
+            const [, a, b] = lineMatch;
+            return { a, b };
+        });
+
+        let count = 1;
+        const asNumbers: Record<string, number> = {};
+        const map: Record<string, string[]> = {};
+        const numberMap = new Map<number, number[]>();
+        for (const { a, b } of caves) {
+            !map[a] && (map[a] = []);
+            !map[b] && (map[b] = []);
+            !map[a].includes(b) && map[a].push(b);
+
+            !asNumbers[a] && (asNumbers[a] = count++ * (a[0].toUpperCase() === a[0] ? 1 : -1));
+            !asNumbers[b] && (asNumbers[b] = count++ * (b[0].toUpperCase() === b[0] ? 1 : -1));
+            !numberMap.has(asNumbers[a]) && numberMap.set(asNumbers[a], []);
+            !numberMap.has(asNumbers[b]) && numberMap.set(asNumbers[b], []);
+            numberMap.get(asNumbers[a])!.push(asNumbers[b]);
+        }
+
+        // console.log(map);
+        // console.log(asNumbers);
+        // console.log(numberMap);
+
+        return {
+            caves,
+            numberMap: numberMap,
+            cavesAsNumbers: asNumbers,
+            start: asNumbers['start'],
+            end: asNumbers['end']
+        };
     }
 }
--- src/tests/solutions/day12.spec.ts
+++ src/tests/solutions/day12.spec.ts
@@ -4,7 +4,7 @@ import { fixtureSetup } from '../testHelper';
 describe('Day12 solution', function () {
     const { setupSolution, expectedResult } = fixtureSetup(Day12);
 
-    describe('for test input', function () {
+    describe('for test input 1', function () {
         beforeAll(setupSolution(`
 start-A
 start-b
@@ -15,6 +15,49 @@ A-end
 b-end`));
 
         it('solves part 1', expectedResult(1, '10'));
+        xit('solves part 2', expectedResult(2, 'Day12Part2'));
+    });
+
+
+    describe('for test input 2', function () {
+        beforeAll(setupSolution(`
+dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc`));
+
+        it('solves part 1', expectedResult(1, '19'));
+        xit('solves part 2', expectedResult(2, 'Day12Part2'));
+    });
+
+    describe('for test input 3', function () {
+        beforeAll(setupSolution(`
+fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW`));
+
+        it('solves part 1', expectedResult(1, '226'));
         xit('solves part 2', expectedResult(2, 'Day12Part2'));
     });
 
