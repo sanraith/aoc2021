92405ccddab88d69e324f73a034c73bba312a1d8
Soma Zsják <zsjaks@gmail.com>
2021-12-14 19:18:09 +0100

    Cleaned up day 14 solution. Added placeholder doodle for day 14.

--- src/solutions/day14.ts
+++ src/solutions/day14.ts
@@ -2,7 +2,7 @@ import { regexMatches } from '../core/helpers';
 import SolutionBase from '../core/solutionBase';
 import { solutionInfo } from '../core/solutionInfo';
 
-type Rule = { pair: string; newElement: string; };
+type Rule = { pair: string; newLetter: string; newPairs: string[]; };
 
 @solutionInfo({
     day: 14,
@@ -12,87 +12,61 @@ export class Day14 extends SolutionBase {
 
     protected part1(): number {
         const { template, rules } = this.parseInput();
+        const { leastCommonCount, mostCommonCount } = this.runSteps(template, rules, 10);
 
-        let polymer = template;
-        for (let i = 0; i < 10; i++) {
-            this.updateProgress(i / 10);
-            polymer = this.step(polymer, rules);
-        }
-
-        const commonality = Object
-            .entries(polymer.split('').reduce((a, x) => {
-                if (!a[x]) { a[x] = 0; }
-                a[x]++;
-                return a;
-            }, {} as Record<string, number>))
-            .map(([char, count]) => ({ char, count }))
-            .sort((a, b) => a.count - b.count);
-        const leastCommon = commonality[0].count;
-        const mostCommon = commonality[commonality.length - 1].count;
-
-        return mostCommon - leastCommon;
+        return mostCommonCount - leastCommonCount;
     }
 
     protected part2(): number {
         const { template, rules } = this.parseInput();
-        const letters = [...new Set([...rules.flatMap(x => x.pair.split('')), ...rules.map(x => x.newElement)])];
-        const combinations = letters.flatMap(a => letters.map(b => a + b)).sort((a, b) => a.localeCompare(b));
-        const letterCounts = Object.fromEntries(letters.map(x => [x, 0]));
-        const combinationCounts = Object.fromEntries(combinations.map(x => [x, 0]));
+        const { leastCommonCount, mostCommonCount } = this.runSteps(template, rules, 40);
 
-        template.split('').forEach((c, i, a) => {
-            letterCounts[c]++;
-            if (i < a.length - 1) {
-                combinationCounts[c + a[i + 1]]++;
+        return mostCommonCount - leastCommonCount;
+    }
+
+    private runSteps(template: string, rules: Rule[], stepCount: number) {
+        const letters = [...new Set(rules.flatMap(x => [x.newLetter, ...x.pair.split('')]))];
+        const letterCounts = Object.fromEntries(letters.map(x => [x, 0]));
+        const pairCombinations = letters.flatMap(a => letters.map(b => a + b));
+        const pairCounts = Object.fromEntries(pairCombinations.map(x => [x, 0]));
+        const ruleMap = Object.fromEntries(rules.map(rule => ([rule.pair, rule])));
+
+        // Populate letter and combination counts from template
+        template.split('').forEach((char, i, templateArray) => {
+            letterCounts[char]++;
+            if (i < templateArray.length - 1) {
+                pairCounts[char + templateArray[i + 1]]++;
             }
         });
 
-        const extendedRules = Object.fromEntries(rules.map(r => ({
-            ...r,
-            affects: [r.pair[0] + r.newElement, r.newElement + r.pair[1]]
-        })).map(r => [r.pair, r]));
-        for (let step = 0; step < 40; step++) {
-            for (const [key, count] of Object.entries(combinationCounts).filter(([, count]) => count > 0)) {
-                const { affects, newElement } = extendedRules[key];
-                if (!affects) { continue; }
-
-                letterCounts[newElement] += count;
-                combinationCounts[key] -= count;
-                affects.forEach(x => {
-                    combinationCounts[x] += count;
-                });
+        // Run steps
+        for (let step = 0; step < stepCount; step++) {
+            for (const [pair, count] of Object.entries(pairCounts)) {
+                const { newPairs, newLetter } = ruleMap[pair];
+                letterCounts[newLetter] += count;
+                pairCounts[pair] -= count;
+                newPairs.forEach(newPair => pairCounts[newPair] += count);
             }
         }
 
-        const commonality = Object.entries(letterCounts)
+        // Read commonality
+        const letterCountsAscending = Object.entries(letterCounts)
             .map(([char, count]) => ({ char, count }))
             .sort((a, b) => a.count - b.count);
+        const leastCommonCount = letterCountsAscending[0].count;
+        const mostCommonCount = letterCountsAscending[letterCountsAscending.length - 1].count;
 
-        const leastCommon = commonality[0].count;
-        const mostCommon = commonality[commonality.length - 1].count;
-
-        return mostCommon - leastCommon;
-    }
-
-    private step(polymer: string, rules: Rule[]) {
-        const area: string[] = [];
-        for (let i = 0; i < polymer.length - 1; i++) {
-            const rule = rules.find(r => r.pair[0] === polymer[i] && r.pair[1] === polymer[i + 1]);
-            if (rule) {
-                area.push(polymer[i], rule.newElement);
-            } else {
-                area.push(polymer[i]);
-            }
-        }
-        area.push(polymer[polymer.length - 1]);
-
-        return area.join('');
+        return { mostCommonCount, leastCommonCount };
     }
 
     private parseInput() {
-        const template = (this.input).match(/^\w+/m)?.[0] ?? '';
+        const template = this.input.match(/^\w+/m)?.[0] ?? '';
         const rules = regexMatches(/(\w{2}) -> (\w)/g, this.input)
-            .map(([, pair, newElement]) => ({ pair, newElement }));
+            .map(([, pair, newLetter]) => ({
+                pair,
+                newLetter,
+                newPairs: [pair[0] + newLetter, newLetter + pair[1]]
+            }));
 
         return { template, rules };
     }
