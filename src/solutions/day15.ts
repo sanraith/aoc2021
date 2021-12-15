97e3a220f1f1433098eb570f82b2b11f9e174120
Soma Zsják <zsjaks@gmail.com>
2021-12-15 16:15:37 +0100

    Added day 15 part 1 solution.

--- src/solutions/day15.ts
+++ src/solutions/day15.ts
@@ -1,5 +1,18 @@
+import { regexMatches } from '../core/helpers';
 import SolutionBase from '../core/solutionBase';
 import { solutionInfo } from '../core/solutionInfo';
+
+type Grid = { map: number[][]; width: number; height: number; };
+class Point {
+    constructor(public x: number, public y: number) { }
+    add(other: Point): Point { return new Point(this.x + other.x, this.y + other.y); }
+    manhattan(other: Point): number { return Math.abs(this.x - other.x) + Math.abs(this.y - other.y); }
+    isInBounds(width: number, height: number) {
+        return this.x >= 0 && this.x < width && this.y >= 0 && this.y < height;
+    }
+    toString(): string { return `${this.x},${this.y}`; }
+}
+const directions: readonly Point[] = [[1, 0], [0, 1], [-1, 0], [0, -1]].map(([x, y]) => new Point(x, y));
 
 @solutionInfo({
     day: 15,
@@ -8,10 +21,60 @@ import { solutionInfo } from '../core/solutionInfo';
 export class Day15 extends SolutionBase {
 
     protected part1(): number {
-        this.noSolution();
+        const grid = this.parseGrid();
+        const start = new Point(0, 0);
+        const end = new Point(grid.width - 1, grid.height - 1);
+        const distance = this.findBestPath(grid, start, end);
+
+        return distance;
     }
 
     protected part2(): string | number {
-        this.noSolution();
+        const grid = this.enlargeGrid(this.parseGrid(), 5);
+        const start = new Point(0, 0);
+        const end = new Point(grid.width - 1, grid.height - 1);
+        const distance = this.findBestPath(grid, start, end);
+
+        return distance;
+    }
+
+    private enlargeGrid(grid: Grid, times: number): Grid {
+        return grid; // TODO
+    }
+
+    private findBestPath(grid: Grid, start: Point, end: Point) {
+        const { map, width, height } = grid;
+        const distances: number[][] = Array(height).fill(0).map(() => Array(width).fill(-1));
+        const visited = new Set<string>([end.toString()]);
+        const queue = [end];
+        while (queue.length > 0) {
+            const point = queue.shift()!;
+
+            let bestDistance = undefined;
+            for (const next of directions.map(d => point.add(d))) {
+                if (!next.isInBounds(width, height)) { continue; }
+                const distance = distances[next.y][next.x];
+                if (distance >= 0 && (bestDistance === undefined || distance < bestDistance)) {
+                    bestDistance = distance;
+                }
+
+                const nextStr = next.toString();
+                if (!visited.has(nextStr)) {
+                    visited.add(nextStr);
+                    queue.push(next);
+                }
+            }
+            distances[point.y][point.x] = map[point.y][point.x] + (bestDistance ?? 0);
+        }
+
+        return distances[start.y][start.x] - map[start.y][start.x];
+    }
+
+    private parseGrid() {
+        const map = regexMatches(/\d+/g, this.input).map(([line]) =>
+            line.split('').map(x => parseInt(x))
+        );
+
+        return { map, width: map[0].length, height: map.length };
     }
 }
--- src/tests/solutions/day15.spec.ts
+++ src/tests/solutions/day15.spec.ts
@@ -18,13 +18,13 @@ describe('Day15 solution', function () {
 2311944581`));
 
         it('solves part 1', expectedResult(1, '40'));
-        xit('solves part 2', expectedResult(2, 'Day15Part2'));
+        it('solves part 2', expectedResult(2, '315'));
     });
 
     describe('for puzzle input', function () {
         beforeAll(setupSolution());
 
-        xit('solves part 1', expectedResult(1, 'Day15Part1'));
+        it('solves part 1', expectedResult(1, '447'));
         xit('solves part 2', expectedResult(2, 'Day15Part2'));
     });
 });
