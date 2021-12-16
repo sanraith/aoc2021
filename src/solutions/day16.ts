8874c10cc7319e86300a63b388a7fbb1b4a1bd40
Soma Zsják <sanraith@users.noreply.github.com>
2021-12-16 13:19:25 +0100

    Added day 16 part 2 solution.

--- src/solutions/day16.ts
+++ src/solutions/day16.ts
@@ -2,9 +2,8 @@ import { regexMatches } from '../core/helpers';
 import SolutionBase from '../core/solutionBase';
 import { solutionInfo } from '../core/solutionInfo';
 
-interface PacketBase { version: number; }
-interface LiteralPacket extends PacketBase { kind: 'literal'; value: number; }
-interface OperatorPacket extends PacketBase { kind: 'operator'; operator: number; packets: Packet[]; }
+type LiteralPacket = { kind: 'literal'; version: number; value: number; };
+type OperatorPacket = { kind: 'operator'; version: number; operator: number; packets: Packet[]; };
 type Packet = LiteralPacket | OperatorPacket;
 
 @solutionInfo({
@@ -18,79 +17,91 @@ export class Day16 extends SolutionBase {
         return flatPackets.reduce((a, x) => a + x.version, 0);
     }
 
-    protected part2(): string | number {
-        this.noSolution();
+    protected part2(): number {
+        const { rootPacket } = this.parseInput();
+        return this.evaluate(rootPacket);
+    }
+
+    private evaluate(packet: Packet): number {
+        if (packet.kind === 'literal') {
+            return packet.value;
+        }
+
+        const packets = packet.packets;
+        switch (packet.operator) {
+            case 0: return packets.reduce((a, x) => a + this.evaluate(x), 0); // sum
+            case 1: return packets.reduce((a, x) => a * this.evaluate(x), 1); // product
+            case 2: return packets.map(x => this.evaluate(x)).reduce((a, x) => x < a ? x : a); // minimum
+            case 3: return packets.map(x => this.evaluate(x)).reduce((a, x) => x > a ? x : a); // maximum
+            case 5: return this.evaluate(packets[0]) > this.evaluate(packets[1]) ? 1 : 0; // greater than
+            case 6: return this.evaluate(packets[0]) < this.evaluate(packets[1]) ? 1 : 0; // less than
+            case 7: return this.evaluate(packets[0]) === this.evaluate(packets[1]) ? 1 : 0; // equal
+            default: throw new Error(`Invalid operator: ${packet.operator}`);
+        }
     }
 
     private parseInput() {
-        console.log();
-        const hex = /[A-F0-9]+/.exec(this.input)?.[0] ?? '';
+        const hex = /[0-9A-F]+/.exec(this.input)?.[0] ?? '';
         const binary = hex.split('').map(x => parseInt(x, 16).toString(2).padStart(4, '0')).join('');
-        return this.parsePacket(binary);
+        const flatPackets: Packet[] = [];
+        return { rootPacket: this.parsePacket(binary, flatPackets).packet, flatPackets };
     }
 
-    private parsePacket(binary: string, flatPackets?: Packet[]) {
-        flatPackets ??= [];
+    private parsePacket(binary: string, flatPackets: Packet[]) {
         const literalPacket = /^(?<version>\d{3})100(?<data>(?:1\d{4})*0\d{4})/;
         const lengthPacketHeader = /^(?<version>\d{3})(?<operator>(?!100)\d{3})0(?<length>\d{15})/;
         const countPacketHeader = /^(?<version>\d{3})(?<operator>(?!100)\d{3})1(?<count>\d{11})/;
 
         let match: RegExpExecArray | null;
-        let packet: Packet;
-        let readLength = 0;
         if ((match = literalPacket.exec(binary))) {
-            const { version, data } = match.groups!;
-
-            packet = <LiteralPacket>{
-                kind: 'literal',
-                version: parseInt(version, 2),
-                value: parseInt(regexMatches(/\d{5}/g, data).map(x => x[0].slice(1)).join(''), 2)
-            };
-            readLength += match[0].length;
-
+            return this.parseLiteralPacket(match, flatPackets);
         } else if ((match = lengthPacketHeader.exec(binary))) {
-            const { version, operator, length } = match.groups!;
-            const innerLengthTotal = parseInt(length, 2);
-            const headerLength = match[0].length;
-            let innerStart = headerLength;
-            packet = <OperatorPacket>{
-                kind: 'operator',
-                version: parseInt(version, 2),
-                operator: parseInt(operator, 2),
-                packets: []
-            };
-
-            while (innerStart - headerLength < innerLengthTotal) {
-                const { packet: innerPacket, length: innerLength } = this.parsePacket(binary.slice(innerStart), flatPackets);
-                innerStart += innerLength;
-                packet.packets.push(innerPacket);
-
-            }
-            readLength += innerStart;
-
+            return this.parseOperatorPacket(binary, match, flatPackets);
         } else if ((match = countPacketHeader.exec(binary))) {
-            const { version, operator, count } = match.groups!;
-            const packetCount = parseInt(count, 2);
-            packet = <OperatorPacket>{
-                kind: 'operator',
-                version: parseInt(version, 2),
-                operator: parseInt(operator, 2),
-                packets: []
-            };
-
-            let innerStart = match[0].length;
-            for (let i = 0; i < packetCount; i++) {
-                const { packet: innerPacket, length: innerLength } = this.parsePacket(binary.slice(innerStart), flatPackets);
-                innerStart += innerLength;
-                packet.packets.push(innerPacket);
-            }
-            readLength += innerStart;
-
-        } else {
-            throw new Error();
+            return this.parseOperatorPacket(binary, match, flatPackets);
         }
 
+        throw new Error(`Unknown packet format: ${binary}`);
+    }
+
+    private parseLiteralPacket(match: RegExpExecArray, flatPackets: Packet[]) {
+        const { version, data } = match.groups!;
+        const packet = <LiteralPacket>{
+            kind: 'literal',
+            version: parseInt(version, 2),
+            value: parseInt(regexMatches(/\d{5}/g, data).map(x => x[0].slice(1)).join(''), 2)
+        };
         flatPackets.push(packet);
-        return { packet, length: readLength, flatPackets };
+
+        return { packet, length: match[0].length };
+    }
+
+    private parseOperatorPacket(binary: string, match: RegExpExecArray, flatPackets: Packet[]) {
+        const { version, operator, length, count } = match.groups!;
+        const headerLength = match[0].length;
+        const packet = <OperatorPacket>{
+            kind: 'operator',
+            version: parseInt(version, 2),
+            operator: parseInt(operator, 2),
+            packets: []
+        };
+        flatPackets.push(packet);
+
+        const parseInnerPacket = (from: number) => {
+            const { packet: innerPacket, length: innerLength } = this.parsePacket(binary.slice(from), flatPackets);
+            packet.packets.push(innerPacket);
+            return innerLength;
+        };
+
+        let packetLength = headerLength;
+        if (length !== undefined) {
+            const innerLengthTotal = parseInt(length, 2);
+            while (packetLength - headerLength < innerLengthTotal) { packetLength += parseInnerPacket(packetLength); }
+        } else {
+            const packetCount = parseInt(count, 2);
+            for (let i = 0; i < packetCount; i++) { packetLength += parseInnerPacket(packetLength); }
+        }
+
+        return { packet, length: packetLength };
     }
 }
--- src/tests/solutions/day16.spec.ts
+++ src/tests/solutions/day16.spec.ts
@@ -4,33 +4,63 @@ import { fixtureSetup } from '../testHelper';
 describe('Day16 solution', function () {
     const { setupSolution, expectedResult } = fixtureSetup(Day16);
 
-    describe('for test input 1', function () {
-        beforeAll(setupSolution('8A004A801A8002F478'));
-        it('solves part 1', expectedResult(1, '16'));
-        xit('solves part 2', expectedResult(2, 'Day16Part2'));
+    describe('in part 1 inputs', function () {
+        describe('for test input 1', function () {
+            beforeAll(setupSolution('8A004A801A8002F478'));
+            it('solves part 1', expectedResult(1, '16'));
+        });
+        describe('for test input 2', function () {
+            beforeAll(setupSolution('620080001611562C8802118E34'));
+            it('solves part 1', expectedResult(1, '12'));
+        });
+        describe('for test input 3', function () {
+            beforeAll(setupSolution('C0015000016115A2E0802F182340'));
+            it('solves part 1', expectedResult(1, '23'));
+        });
+        describe('for test input 4', function () {
+            beforeAll(setupSolution('A0016C880162017C3686B18A3D4780'));
+            it('solves part 1', expectedResult(1, '31'));
+        });
     });
 
-    describe('for test input 2', function () {
-        beforeAll(setupSolution('620080001611562C8802118E34'));
-        it('solves part 1', expectedResult(1, '12'));
-        xit('solves part 2', expectedResult(2, 'Day16Part2'));
-    });
-
-    describe('for test input 3', function () {
-        beforeAll(setupSolution('C0015000016115A2E0802F182340'));
-        it('solves part 1', expectedResult(1, '23'));
-        xit('solves part 2', expectedResult(2, 'Day16Part2'));
-    });
-
-    describe('for test input 4', function () {
-        beforeAll(setupSolution('A0016C880162017C3686B18A3D4780'));
-        it('solves part 1', expectedResult(1, '31'));
-        xit('solves part 2', expectedResult(2, 'Day16Part2'));
+    describe('in part 2 inputs', function () {
+        describe('for "sum" test input', function () {
+            beforeAll(setupSolution('C200B40A82'));
+            it('solves part 2', expectedResult(2, '3'));
+        });
+        describe('for "product" test input', function () {
+            beforeAll(setupSolution('04005AC33890'));
+            it('solves part 2', expectedResult(2, '54'));
+        });
+        describe('for "minimum" test input', function () {
+            beforeAll(setupSolution('880086C3E88112'));
+            it('solves part 2', expectedResult(2, '7'));
+        });
+        describe('for "maximum" test input', function () {
+            beforeAll(setupSolution('CE00C43D881120'));
+            it('solves part 2', expectedResult(2, '9'));
+        });
+        describe('for "less than" test input', function () {
+            beforeAll(setupSolution('D8005AC2A8F0'));
+            it('solves part 2', expectedResult(2, '1'));
+        });
+        describe('for "greater than" test input', function () {
+            beforeAll(setupSolution('F600BC2D8F'));
+            it('solves part 2', expectedResult(2, '0'));
+        });
+        describe('for "equal" test input', function () {
+            beforeAll(setupSolution('9C005AC2F8F0'));
+            it('solves part 2', expectedResult(2, '0'));
+        });
+        describe('for complex test input', function () {
+            beforeAll(setupSolution('9C0141080250320F1802104A08'));
+            it('solves part 2', expectedResult(2, '1'));
+        });
     });
 
     describe('for puzzle input', function () {
         beforeAll(setupSolution());
         it('solves part 1', expectedResult(1, '917'));
-        xit('solves part 2', expectedResult(2, 'Day16Part2'));
+        it('solves part 2', expectedResult(2, '2536453523344'));
     });
 });
