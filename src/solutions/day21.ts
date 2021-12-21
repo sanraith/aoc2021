0f20b56d2ae7f6bcda367d687876e208da17bb92
Soma Zsják <sanraith@users.noreply.github.com>
2021-12-21 09:58:21 +0100

    Added day 21 part 1 solution. Set "prefer-const" eslint rule to "warn".

--- src/solutions/day21.ts
+++ src/solutions/day21.ts
@@ -1,3 +1,4 @@
+import { regexMatches } from '../core/helpers';
 import SolutionBase from '../core/solutionBase';
 import { solutionInfo } from '../core/solutionInfo';
 
@@ -8,10 +9,44 @@ import { solutionInfo } from '../core/solutionInfo';
 export class Day21 extends SolutionBase {
 
     protected part1(): number {
+        const players = this.parseInput();
+        const boardSize = 10;
+        const diceSides = 100;
+        const rollsPerTurn = 3;
+        const winningPoints = 1000;
+
+        let playerIndex = 1;
+        let diceValue = 1;
+        let rollCount = 0;
+        while (players[playerIndex].points < winningPoints) {
+            let rollResult = 0;
+            for (let i = 0; i < rollsPerTurn; i++) {
+                diceValue = (diceValue - 1) % diceSides + 1;
+                rollResult += diceValue;
+                diceValue++;
+                rollCount++;
+            }
+
+            playerIndex = (playerIndex + 1) % 2;
+            const player = players[playerIndex];
+            player.pos = (player.pos + rollResult - 1) % boardSize + 1;
+            player.points += player.pos;
+            // console.log(`Player ${player.id} rolls ${rollResult} and moves to space ${player.pos} for a total score of ${player.points}`);
+        }
+
+        return players[(playerIndex + 1) % 2].points * rollCount;
+    }
+
+    protected part2(): number {
         this.noSolution();
     }
 
-    protected part2(): string | number {
-        this.noSolution();
+    private parseInput() {
+        return regexMatches(/Player (\d) starting position: (\d+)/g, this.input)
+            .map(match => ({
+                id: parseInt(match[1]),
+                pos: parseInt(match[2]),
+                points: 0
+            }));
     }
 }
--- src/tests/solutions/day21.spec.ts
+++ src/tests/solutions/day21.spec.ts
@@ -10,13 +10,13 @@ Player 1 starting position: 4
 Player 2 starting position: 8`));
 
         it('solves part 1', expectedResult(1, '739785'));
-        xit('solves part 2', expectedResult(2, 'Day21Part2'));
+        it('solves part 2', expectedResult(2, '444356092776315'));
     });
 
     describe('for puzzle input', function () {
         beforeAll(setupSolution());
 
-        xit('solves part 1', expectedResult(1, 'Day21Part1'));
+        it('solves part 1', expectedResult(1, '797160'));
         xit('solves part 2', expectedResult(2, 'Day21Part2'));
     });
 });
