d81893a25fcc060cc016330f58f2b74f16cb4d09
Soma Zsják <sanraith@users.noreply.github.com>
2021-12-22 10:01:06 +0100

    Added day 22 part 1 solution.

--- src/solutions/day22.ts
+++ src/solutions/day22.ts
@@ -1,3 +1,4 @@
+import { regexMatches } from '../core/helpers';
 import SolutionBase from '../core/solutionBase';
 import { solutionInfo } from '../core/solutionInfo';
 
@@ -8,10 +9,39 @@ import { solutionInfo } from '../core/solutionInfo';
 export class Day22 extends SolutionBase {
 
     protected part1(): number {
+        const { initSteps } = this.parseInput();
+        const coordStr = (x: number, y: number, z: number) => x + ',' + y + ',' + z;
+
+        const map = new Map<string, boolean>();
+        initSteps.forEach(({ isOn, x1, x2, y1, y2, z1, z2 }, index) => {
+            this.updateProgress(index / initSteps.length);
+            for (let x = x1; x <= x2; x++) {
+                for (let y = y1; y <= y2; y++) {
+                    for (let z = z1; z <= z2; z++) {
+                        map.set(coordStr(x, y, z), isOn);
+                    }
+                }
+            }
+        });
+
+        return [...map.values()].reduce((a, x) => a + (x ? 1 : 0), 0);
+    }
+
+    protected part2(): number {
         this.noSolution();
     }
 
-    protected part2(): string | number {
-        this.noSolution();
+    private parseInput() {
+        const regions = regexMatches(/(on|off) x=(-?\d+)\.\.(-?\d+),y=(-?\d+)\.\.(-?\d+),z=(-?\d+)\.\.(-?\d+)/g, this.input)
+            .map(([, onOffMatch, ...coordMatches]) => {
+                const isOn = onOffMatch === 'on';
+                const coords = coordMatches.map(x => parseInt(x));
+                const isInitStep = coords.every(x => x >= -50 && x <= 50);
+                const [x1, x2, y1, y2, z1, z2] = coords;
+
+                return { isOn, isInitStep, x1, x2, y1, y2, z1, z2 };
+            });
+
+        return { initSteps: regions.filter(x => x.isInitStep) };
     }
 }
