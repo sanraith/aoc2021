3b5897d8fe7ebc3150251dde4a2eb13063fd9b06
Soma Zsják <sanraith@users.noreply.github.com>
2021-12-23 02:07:23 +0100

    WIP day 22 part 2 solution.

--- src/solutions/day22.ts
+++ src/solutions/day22.ts
@@ -2,6 +2,18 @@ import { regexMatches } from '../core/helpers';
 import SolutionBase from '../core/solutionBase';
 import { solutionInfo } from '../core/solutionInfo';
 
+type Cube = {
+    x1: number; x2: number;
+    y1: number; y2: number;
+    z1: number; z2: number;
+    volume: number;
+};
+type ReCube = {
+    cube: Cube;
+    state: boolean;
+    missing: ReCube[];
+};
+
 @solutionInfo({
     day: 22,
     title: 'Reactor Reboot'
@@ -13,8 +25,9 @@ export class Day22 extends SolutionBase {
         const coordStr = (x: number, y: number, z: number) => x + ',' + y + ',' + z;
 
         const map = new Map<string, boolean>();
-        initSteps.forEach(({ isOn, x1, x2, y1, y2, z1, z2 }, index) => {
+        initSteps.forEach(({ isOn, cube }, index) => {
             this.updateProgress(index / initSteps.length);
+            const { x1, x2, y1, y2, z1, z2 } = cube;
             for (let x = x1; x <= x2; x++) {
                 for (let y = y1; y <= y2; y++) {
                     for (let z = z1; z <= z2; z++) {
@@ -28,7 +41,57 @@ export class Day22 extends SolutionBase {
     }
 
     protected part2(): number {
-        this.noSolution();
+        const { steps } = this.parseInput();
+
+        const reCubes: ReCube[] = [];
+        steps.forEach(({ cube, isOn }, index) => {
+            this.updateProgress(index / steps.length);
+
+            const hits = reCubes
+                .map(reCube => ({ reCube, intersection: this.reIntersect(reCube, cube) as ReCube }))
+                .filter(x => x.intersection);
+
+            for (const { reCube, intersection } of hits) {
+                intersection.state = false;
+                reCube.missing.push(intersection);
+            }
+
+            if (isOn) {
+                reCubes.push({ state: isOn, cube, missing: [] });
+            }
+        });
+
+        return reCubes.reduce((a, x) => a + this.reVolume(x), 0);
+    }
+
+
+    private intersect(a: Cube, b: Cube): Cube | false {
+        const x1 = Math.max(a.x1, b.x1);
+        const x2 = Math.min(a.x2, b.x2);
+        const y1 = Math.max(a.y1, b.y1);
+        const y2 = Math.min(a.y2, b.y2);
+        const z1 = Math.max(a.z1, b.z1);
+        const z2 = Math.min(a.z2, b.z2);
+
+        if (x1 > x2 || y1 > y2 || z1 > z2) { return false; }
+        return { x1, x2, y1, y2, z1, z2, volume: this.volume(x1, x2, y1, y2, z1, z2) };
+    }
+
+    private reIntersect(a: ReCube, b: Cube): ReCube | false {
+        return false;
+    }
+
+    private volume(x1: number, x2: number, y1: number, y2: number, z1: number, z2: number) {
+        return (x2 - x1 + 1) * (y2 - y1 + 1) * (z2 - z1 + 1);
+    }
+
+    private reVolume(reCube: ReCube): number {
+        let volume = reCube.cube.volume;
+        for (const inner of reCube.missing) {
+            volume -= this.reVolume(inner);
+        }
+
+        return volume;
     }
 
     private parseInput() {
@@ -38,10 +101,11 @@ export class Day22 extends SolutionBase {
                 const coords = coordMatches.map(x => parseInt(x));
                 const isInitStep = coords.every(x => x >= -50 && x <= 50);
                 const [x1, x2, y1, y2, z1, z2] = coords;
+                const cube = { x1, x2, y1, y2, z1, z2, volume: this.volume(x1, x2, y1, y2, z1, z2) };
 
-                return { isOn, isInitStep, x1, x2, y1, y2, z1, z2 };
+                return { isOn, isInitStep, cube };
             });
 
-        return { initSteps: regions.filter(x => x.isInitStep) };
+        return { steps: regions, initSteps: regions.filter(x => x.isInitStep) };
     }
 }
